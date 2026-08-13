5cc9242c228f1d3b053ebc4e5e1a9085ac93a701
Augusto Mazocco <augustomazocco@gmail.com>
2026-08-13 18:49:47 -0300

    update home.blade.php

---
+++ resources/views/components/flooter.blade.php
@@ -0,0 +1,5 @@
+<header class="bg-white border-t border-t-2 p-4">
+    <p class="text-center">
+        Criado por Augusto Mazocco. O código fonte está no <a href="#" class="underline">Github</a>
+    </p>
+</header>
