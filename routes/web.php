6ebcd65c90244d66bd0b8e29a531d4deb7f22f08
Augusto Mazocco <augustomazocco@gmail.com>
2026-08-17 10:35:45 -0300

    Ajuste database sessions, add login functions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 
+//SITE
 Route::get( uri:"/", action: [SiteController::class, 'index']);
+
+//LOGIN
+Route::get( uri:"/login", action: [LoginController::class, 'index']);
