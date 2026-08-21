77b94c68b109d69bc6401fca1db07667c5277914
Augusto Mazocco <augustomazocco@gmail.com>
2026-08-21 18:34:33 -0300

    feat: button for add habits

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,43 +1,46 @@
 <x-layout>
-    <main class="max-w-5x1 mx-auto py-10 min-h-[calc(100vh-160px)] px-4">
+<main class="max-w-5x1 mx-auto py-10 px-4 min-h-[80vh] q-full">
 
-    <x-navbar />
-                
-        <div>
-            <h2 class="text-lg mt-8 mb-2">
-                {{ date('d/m/Y') }}
-            </h2>
+<x-navbar />
+            
+    <div class="flex flex-col gap-2 items-start">
+        <h2 class="text-lg mt-8 mb-2 font-bold">
+            {{ \Carbon\Carbon::now()->locale('pt_BR')->translatedFormat('l, d \d\e F'); }}
+        </h2>
 
-            <ul class="flex flex-col gap-2">
-                
-                @forelse($habits as $item)
-                    <li class="habit-shadow-lg p-2 bg-[#FFDAAC]">
-                        <form 
-                            action="{{ route('habits.toggle', $item->id) }}" 
-                            method="POST"
-                            class="flex gap-2 items-center"
-                            id="form-{{ $item->id }}"
-                            >
-                            <input 
-                                type="checkbox" 
-                                class="w-5 h-5" {{ $item->is_completed ? 'checked' : '' }} 
-                                {{ $item->wasCompletedToday() ? 'checked' : '' }}
-                                onchange="document.getElementById('form-{{ $item->id }}').submit()"
-                            />
-                            <p class="font-bold text-lg">
-                                {{ $item->name }}
-                            </p>
-                        </form>
-                    </li>
-                @empty
-                    <p>
-                        Ainda não tem nenhum hábito cadastrado
-                    </p>
-                    <a href="{{ route('habits.create') }}" class="bg-white p-2 border-2">
-                        Cadastre um novo hábito agora
-                    </a>
-                @endforelse
-            </ul>
-        </div>
-    </main>
+        <ul class="flex flex-col gap-2 w-full">
+            
+            @forelse($habits as $item)
+                <li class="habit-shadow-lg p-2 bg-[#FFDAAC]">
+                    <form 
+                        action="{{ route('habits.toggle', $item->id) }}" 
+                        method="POST"
+                        class="flex gap-2 items-center"
+                        id="form-{{ $item->id }}"
+                        >
+                        <input 
+                            type="checkbox" 
+                            class="w-5 h-5" {{ $item->is_completed ? 'checked' : '' }} 
+                            {{ $item->wasCompletedToday() ? 'checked' : '' }}
+                            onchange="document.getElementById('form-{{ $item->id }}').submit()"
+                        />
+                        <p class="font-bold text-lg">
+                            {{ $item->name }}
+                        </p>
+                    </form>
+                </li>
+            @empty
+                <p>
+                    Ainda não tem nenhum hábito cadastrado
+                </p>
+                <a href="{{ route('habits.create') }}" class="bg-white p-2 border-2">
+                    Cadastre um novo hábito agora
+                </a>
+            @endforelse
+        </ul>
+        <a href="{{ route('habits.create') }}" class="p-2 habit-shadow-lg bg-habit-orange habit-btn">
+            + Adicionar
+        </a>
+    </div>
+</main>
 </x-layout>
